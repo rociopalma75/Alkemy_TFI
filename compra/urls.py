a63ce18a61920b040a2ceb4ac8396bcf0d496a42
rociopalma75 <rociopalma75@gmail.com>
2024-04-09 12:25:44 -0300

    Falta editar las urls y views, simplificar el codigo

--- compra/urls.py
+++ compra/urls.py
@@ -3,9 +3,16 @@ from . import views
 
 urlpatterns = [
     path('', views.readProveedores, name="Mostrar como inicio la lista de proveedores"),
-    path('proveedor/registro', views.mostrarCreateProveedor),
+    # path('proveedor/registro', views.mostrarCreateProveedor, name="Muestra el form para crear proveedor"),
+    path('producto/registro', views.mostrarCreateProducto, name="Muestra el form para crear producto"),
     path('proveedores/', views.readProveedores, name="Muestra todos los proveedores"),
     path('productos/', views.readProductos, name="Mostrar todos los productos"),
-    path('proveedor/create', views.createProveedor, name="Crear proveedor nuevo"),
-    path('createProveedor/<str:nombre>/<str:apellido>/<int:dni>', views.createProveedorv2, name="Crear nuevo proveedor"),
+    path('proveedor/create', views.proveedorCreate, name="Metodo POST. Crea proveedor"),
+    path('producto/create', views.createProducto, name="Metodo POST. Crear producto (sin proveedor)"),
+    path('producto/completarCreateProducto', views.completarCreateProducto, name="Metodo POST. Registra el producto con su proveedor"),
+    path('producto/<int:idProducto>/proveedor/registro', views.mostrarCreateProveedorParaProducto, name="Mostrar el form de crear proveedor, luego de agregar producto"),
+    path('proveedor/eliminar/<int:proveedorId>', views.deleteProveedor, name="Metodo DELETE. Elimina proveedor por id"),
+    path('producto/eliminar/<int:productoId>', views.deleteProducto, name="Metodo DELETE. Elimina producto por id"),
+    path('producto/editar/<int:productoId>', views.mostrarEditarProducto, name="Mostrar la vista de editar producto por id"),
+    path('producto/editar', views.editarProducto, name="Metodo POST. Edita producto"),
 ]
--- compra/views.py
+++ compra/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Proveedor, Producto
 from django.template import Template, Context
 
 # Create your views here.
-def mostrarCreateProveedor(request):
-    return render(request, 'crear_proveedor.html')
+
+def mostrarCreateProveedorParaProducto(request, idProducto):
+    return render(request, 'crear_proveedor.html',{'idProducto': idProducto})
+
+def mostrarCreateProducto(request):
+    proveedores = Proveedor.objects.all()
+    return render(request, 'crear_producto.html', {'proveedores': proveedores})
+
+def mostrarEditarProducto(request, productoId):
+    producto = Producto.objects.get(id=productoId)
+    productos = Producto.objects.all()
+    proveedores = Proveedor.objects.all()
+    return render(request, 'editar_producto.html', {'p': producto,'productos':productos, 'proveedores':proveedores})
+
+def readProveedores(request):
+    proveedores = Proveedor.objects.all()
+    return render(request, 'lista_proveedores.html', {'proveedores': proveedores})
+
+def readProductos(request):
+    productos = Producto.objects.all()
+    return render(request, 'lista_productos.html', {'productos': productos})
 
 def createProveedor(request):
     if request.method == 'POST':
@@ -14,32 +33,79 @@ def createProveedor(request):
         prov.apellido = request.POST['apellido']
         prov.dni = request.POST['dni']
         prov.save()
+
+        try:
+            if request.POST['idProducto'] is not None:
+                idProducto = request.POST['idProducto']
+                producto = Producto.objects.get(id=idProducto)
+                proveedores = Proveedor.objects.all()
+                return render(request, 'agregar_proveedor.html', {'producto': producto, 'proveedores': proveedores})
+        except:    
+            return render(request, 'crear_proveedor.html', {'proveedor': prov})
+
+def createProducto(request):
+    if request.method == 'POST':
+        prod = Producto()
+        prod.nombre = request.POST['nombre']
+        prod.precio = request.POST['precio']
+        prod.stock_actual = request.POST['stock_actual']
+        prod.proveedor = None
+        prod.save()
+
+        proveedores = Proveedor.objects.all()
+
+        return render(request, 'agregar_proveedor.html', {'producto': prod, 'proveedores': proveedores})
     
-        return render(request, 'crear_proveedor.html', {'proveedor': prov})
+def completarCreateProducto(request): 
+    productoId = request.POST['id']
+    proveedorId = request.POST['proveedor']
+    proveedor = Proveedor.objects.get(id=proveedorId)
+    producto = Producto.objects.get(id=productoId)
+    producto.proveedor = proveedor
+    producto.save()
 
-def createProveedorv2(request, nombre, apellido, dni):
-    nuevo_proveedor = Proveedor.objects.create(
-        nombre = nombre,
-        apellido = apellido,
-        dni = dni
-    )
-    ruta = "C:\\Users\\rocio\\OneDrive\\Documentos\\Alkemy-Trabajo_Integrador_Individual\\StockControl\\compra\\templates\\nuevo_proveedor.html"
-    doc_externo = open(ruta)
-    plantilla = Template(doc_externo.read())
-    doc_externo.close()
-    contexto = Context({'proveedor' : nuevo_proveedor})
-    return HttpResponse(plantilla.render(contexto))
+    return render(request, 'crear_producto.html',{'producto': producto})
 
-def readProveedores(request):
-    proveedores = Proveedor.objects.all()
-    return render(request, 'lista_proveedores.html', {'proveedores': proveedores})
 
-def deleteProveedor(request, id):
-    proveedor = Proveedor.objects.get(id=id)
+def deleteProveedor(request, proveedorId):
+    proveedor = Proveedor.objects.get(id=proveedorId)
     proveedor.delete()
-    proveedores = Proveedor.objects.all()
-    return render(request, 'lista_proveedores', {'proveedores': proveedores})
+    return redirect('/')
 
-def readProductos(request):
-    productos = Producto.objects.all()
-    return render(request, 'lista_productos.html', {'productos': productos})
+def deleteProducto(request, productoId):
+    producto = Producto.objects.get(id=productoId)
+    producto.delete()
+    return redirect('/productos')
+
+def editarProducto(request):
+    if request.method == 'POST':
+        productoId = request.POST['idProducto']
+        producto = Producto.objects.get(id=productoId)
+        producto.nombre = request.POST['nombre']
+        producto.precio = request.POST['precio']
+        producto.stock_actual = request.POST['stock_actual']
+        if request.POST['proveedor']:
+            proveedorId = request.POST['proveedor']
+            proveedor = Proveedor.objects.get(id = proveedorId)
+            producto.proveedor = proveedor
+        producto.save()
+    
+    return redirect('/productos')
+
+def proveedorCreate(request):
+    if request.method == 'POST':
+        prov = Proveedor()
+        prov.nombre = request.POST['nombre']
+        prov.apellido = request.POST['apellido']
+        prov.dni = request.POST['dni']
+        prov.save()
+        try:
+            if request.POST['idProducto'] is not None:
+                idProducto = request.POST['idProducto']
+                producto = Producto.objects.get(id=idProducto)
+                proveedores = Proveedor.objects.all()
+                return render(request, 'agregar_proveedor.html', {'producto': producto, 'proveedores': proveedores})
+        except:    
+            return render(request, 'crear_proveedor.html', {'proveedor': prov})
+    else: # Metodo GET
+        return render(request, 'crear_proveedor.html')
